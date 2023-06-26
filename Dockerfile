1ddd64b606cd52a7aebfcfb629e4c887f7726db5
Chase Yakaboski <chase.th@dartmouth.edu>
2023-06-26 12:50:35 -0400

    Updated dockerfile and init.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM julia:1.9
 
-RUN apt-get update && apt-get install -y time python3-pip
+RUN apt-get update && apt-get install -y time python3-dev build-essential python3-pip
 
 # add app user
 RUN groupadd gennifer_user && useradd -ms /bin/bash -g gennifer_user gennifer_user
@@ -11,7 +11,7 @@ WORKDIR /app
 COPY ./requirements.txt /app
 
 # Install the required packages
-RUN pip3 install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir --break-system-packages -r requirements.txt
 
 # chown all the files to the app user
 RUN chown -R gennifer_user:gennifer_user /app
--- pidc/__init__.py
+++ pidc/__init__.py
@@ -70,6 +70,7 @@ def create_app(test_config=None):
                     "description": 'PIDC is available in a Julia package titled “NetworkInference”. This package has options to compute various network inference algorithms such has Mutual Information, CLR, PUC and PIDC. We used the maximum likelihood estimator, as recommended by the authors. We did not use any edge-weight cut-off. We converted the resulting undircted network in the form of an adjacency matrix into to a ranked edge list.',
                     "edge_weight_type": 'maximum_liklihood_estimator',
                     "edge_weight_description": "",
+                    "directed": False,
                     }
             return info, 200
 
