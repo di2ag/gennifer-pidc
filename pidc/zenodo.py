92e51ab3cdcacbf8f87fb8ab2512fa1a8c4b2de3
CHP Dev Machine User (Chase, Greg, or Anthony) <di2ag.thayer.dartmouth@gmail.com>
2024-02-15 20:47:31 +0000

    updated to new zenodo API.

--- pidc/zenodo.py
+++ pidc/zenodo.py
@@ -2,16 +2,18 @@ import requests
 import csv
 import io
 import pandas as pd
+import os
 
 def load_file(zenodo_id, file_key, base_url="https://zenodo.org/api/records"):
     r = requests.get(f"{base_url}/{zenodo_id}").json()
     files = {f["key"]: f for f in r["files"]}
     f = files[file_key]
     download_link = f["links"]["self"]
-    file_type = f["type"]
-    if file_type == 'json':
+    # Deduce type from extension
+    _, file_type = os.path.splitext(f['key'])
+    if file_type == '.json':
         return requests.get(download_link).json()
-    if file_type  == 'csv':
+    if file_type  == '.csv':
         with requests.Session() as s:
             download = s.get(download_link)
             decoded_content = download.content.decode('utf-8')
